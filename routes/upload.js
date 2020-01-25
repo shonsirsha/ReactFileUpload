87bcafd5fffe135df98c0513550bdee64227489e
Sean Saoirse <seansaoirse@guo.jing_android>
2020-01-25 21:19:58 +0100

    react installed

---
+++ routes/upload.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/", (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: "No file was uploaded." }); // bad req
+  }
+
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ msg: `Server error ${err}` }); //server err
+    }
+
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+
+const app = express();
+app.use(fileUpload());
+
+//upload endpoint
+
+app.use("/api/upload", require("./routes/upload"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
